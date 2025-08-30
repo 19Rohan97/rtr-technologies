1d7ab8b80e39883f57a3647a8af25fbb4411c3bf
Rohan T George <67993133+19Rohan97@users.noreply.github.com>
2025-08-30 10:23:35 -0400

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,6 @@ export default function RootLayout({
           )}
           {children}
           <VercelAnalytics />
-          {/* Mobile bottom navigation */}
           <MobileNav />
         </ThemeProvider>
       </body>
